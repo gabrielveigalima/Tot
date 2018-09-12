ac5ac87b2659ea44ec57c876945ba640ba243def
gabrielveigalima <gabrielveigalima@icloud.com>
2018-09-11 23:09:52 -0300

    Tot já está ouvindo com a api do google em português

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+# encoding: utf-8
+                                                                          
+import pyaudio
 import speech_recognition as sr
 
-r = sr.Recognizer()
-
-with sr.Microphone() as s:
-    r.adjust_for_ambient_noise(s)
-    while True:
-        audio = r.listen(s)
-
-        speech = r.recognize_google(audio, language='pt')
-        print('Você disse: ', speech)
-
+# pega audio pelo microfone
+r = sr.Recognizer()                                                                                   
+with sr.Microphone() as source:                                                                       
+    print("Estou ouvindo... :)")
+    audio = r.listen(source)   
+try:
+    print("Você disse " + r.recognize_google(audio, language='pt'))
+except sr.UnknownValueError:
+    print("Could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results; {0}".format(e))
---
+++ teste.py
@@ -0,0 +1,3 @@
+import pyttsx
+
+pyttsx.init()
