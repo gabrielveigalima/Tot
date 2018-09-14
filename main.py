8e026c4cb5de96cbacd813f9d3e883aa8fe70765
gabrielveigalima <gabrielveigalima@icloud.com>
2018-09-14 20:02:32 -0300

    Limpando o projeto, tirando aquivos obsoletos

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ fala = "sim".upper()
 # pega audio pelo microfone
 r = sr.Recognizer()
 with sr.Microphone() as source:
-    print("Estou ouvindo... :)")
-    audio = r.listen(source)
 
+    print("Olá!\nEstou ouvindo... :)\nCaso queria sair, diga 'sair'")
+
+    audio = r.listen(source)
 try:
-    print("Você disse " + r.recognize_google(audio, language='pt'))
+    fala = r.recognize_google(audio, language='pt')
+    print("Você disse " + fala)
+    
 except sr.UnknownValueError:
+
     print("Não foi possível entender o áudio")
 except sr.RequestError as e:
     print("Não foi possível solicitar resultados. {0}".format(e))
--- teste.py
+++
@@ -1,3 +0,0 @@
-import pyttsx
-
-pyttsx.init()
