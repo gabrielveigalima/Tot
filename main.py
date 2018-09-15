0569d9ae759a4e1ffaeca61bc8a3bc44e3118cff
gabrielveigalima <gabrielveigalima@icloud.com>
2018-09-14 21:05:56 -0300

    Tot está ouvindo varias vezes até eu dar o camando para sair

--- main.py
+++ main.py
@@ -6,16 +6,20 @@ fala = "sim".upper()
 # pega audio pelo microfone
 r = sr.Recognizer()
 with sr.Microphone() as source:
+    r.adjust_for_ambient_noise(source)
+    print("Tot disse: Fala meu Padrinho!\nEstou ouvindo... :)\nCaso queria sair, diga 'tchau Tot'")
 
-    print("Olá!\nEstou ouvindo... :)\nCaso queria sair, diga 'sair'")
-
-    audio = r.listen(source)
-try:
-    fala = r.recognize_google(audio, language='pt')
-    print("Você disse " + fala)
-    
-except sr.UnknownValueError:
-
-    print("Não foi possível entender o áudio")
-except sr.RequestError as e:
-    print("Não foi possível solicitar resultados. {0}".format(e))
+    while True:
+        audio = r.listen(source)
+        try:
+            fala = r.recognize_google(audio, language='pt')
+            if(fala.lower() == 'tchau tot' or fala.lower() == 'tchau toddy' or fala.lower() == 'tchau' or fala.lower() == 'tchau totti'):
+                print('Tot disse: Tchau... :)')
+                break
+            else:
+                print("Você disse: " + fala)
+            
+        except sr.UnknownValueError:
+            print('Tot disse: Não foi possível entender o áudio')
+        except sr.RequestError as e:
+            print('Tot disse: Não foi possível solicitar resultados. {0}'.format(e))
