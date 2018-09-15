b2706232c1777ff109cd64e528a4764fb10a3a30
gabrielveigalima <gabrielveigalima@icloud.com>
2018-09-15 11:39:20 -0300

    Tot já está interagindo

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-# encoding: utf-8
+#-*- coding: utf-8 -*-
                                                                           
-#from chatterbot.trainers import ListTrainer
-#from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from chatterbot import ChatBot
 
 import os
 
 import pyaudio
 import speech_recognition as sr
 
-#bot = ChatBot('Tot', read_only=True)
+bot = ChatBot('Tot', read_only=True)
 
-""" treinar Tot
-bot.set_trainer(ListTrainer)
+#treinar Tot
+"""bot.set_trainer(ListTrainer)
 
 for arquivo in os.listdir('conversas'): #percorrer todos os aquivos do diretório 
     linhas = open('conversas/'+ arquivo, 'r').readlines() #ler as linhas do arquivo 
@@ -29,15 +29,15 @@ with sr.Microphone() as source:
 
     while True:
         audio = r.listen(source)
-        #try:
-        fala = r.recognize_google(audio, language='pt')
-        """if(fala.lower() == 'tchau tot' or fala.lower() == 'tchau toddy' or fala.lower() == 'tchau' or fala.lower() == 'tchau totti'):
-            print('Tot disse: Tchau... :)')
-            break
-        else:"""
-        print("Você disse: " + fala)  
-        print("Tot disse: " + bot.get_response(fala))      
-        """except sr.UnknownValueError:
+        try:
+            fala = r.recognize_google(audio, language='pt')
+            if(fala.lower() == 'tchau tot' or fala.lower() == 'tchau toddy' or fala.lower() == 'tchau' or fala.lower() == 'tchau totti'):
+                print('Tot disse: Tchau... :)')
+                break
+            else:
+                print('Você disse: {0}'.format(fala))  
+                print('Tot  disse: {0}'.format(bot.get_response(fala)))      
+        except sr.UnknownValueError:
             print('Tot disse: Não foi possível entender o áudio')
         except sr.RequestError as e:
-            print('Tot disse: Não foi possível solicitar resultados. {0}'.format(e))"""
+            print('Tot disse: Não foi possível solicitar resultados. {0}'.format(e))
