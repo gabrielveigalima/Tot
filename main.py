2243f0d35d916dbfef38b4b9f260253d2ab50f2c
gabrielveigalima <gabrielveigalima@icloud.com>
2018-09-15 14:22:40 -0300

    Tot já está falando

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import os
 
 import pyaudio
 import speech_recognition as sr
+import pyttsx3
+
+voz = pyttsx3.init('sapi5');
 
 bot = ChatBot('Tot', read_only=True)
 
@@ -19,25 +22,33 @@ for arquivo in os.listdir('conversas'): #percorrer todos os aquivos do diretóri
     bot.train(linhas)"""
 
 
-# pega audio pelo microfone
+def falar(texto):
+    voz.say(texto)
+    voz.runAndWait()
 
+# pega audio pelo microfone
 
 r = sr.Recognizer()
 with sr.Microphone() as source:
     r.adjust_for_ambient_noise(source)
     print("Tot disse: Fala meu Padrinho!\nEstou ouvindo... :)\nCaso queria sair, diga 'tchau Tot'")
-
+    falar('Fala meu Padrinho! Estou ouvindo, caso queria sair, diga tchau Tot')
     while True:
         audio = r.listen(source)
         try:
             fala = r.recognize_google(audio, language='pt')
             if(fala.lower() == 'tchau tot' or fala.lower() == 'tchau toddy' or fala.lower() == 'tchau' or fala.lower() == 'tchau totti'):
-                print('Tot disse: Tchau... :)')
+                print('Tot disse: Tchau Padrinho... :)')
+                falar('Tchau padrinho')
                 break
             else:
                 print('Você disse: {0}'.format(fala))  
-                print('Tot  disse: {0}'.format(bot.get_response(fala)))      
+                respostaTot = bot.get_response(fala)
+                falar(respostaTot)
+                print('Tot  disse: {0}'.format(respostaTot))      
         except sr.UnknownValueError:
             print('Tot disse: Não foi possível entender o áudio')
+            falar('Não foi possível entender o áudio')
         except sr.RequestError as e:
+            falar('Não foi possível entender o áudio, deu erro')
             print('Tot disse: Não foi possível solicitar resultados. {0}'.format(e))
